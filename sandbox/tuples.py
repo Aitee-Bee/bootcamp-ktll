ad282846bc2cc8a0a3af4dc588d9c5bebd423630
Itoro Eti-mfon <etimfonitoro@gmail.com>
2024-06-28 19:16:56 +0100

    extra file

--- sandbox/tuples.py
+++ sandbox/tuples.py
@@ -7,3 +7,28 @@ simply just add it to the list. Then will return the list.
 
 'wilson@gmail.net', 'swedishgee@yahoo.com', 'pelstop@gmail.com', 'deniah@gmail.edu'
 """
+
+email_address = ("'wilson@gmail.net', 'swedishgee@yahoo.com', 'pelstop@gmail.com', 'deniah@gmail.edu'")
+
+
+def check_email(email_address):
+    clean_emails = []
+    for email in email_address:
+        dot = email.index(".")
+        root = email[dot:]
+        if root != ".com":
+            new_email = email[:dot] + ".com"
+            clean_emails.append(new_email)
+        else:
+            clean_emails.append(email)
+    return clean_emails
+
+
+print(check_email('wilson@gmail.net'))
+
+
+
+
+
+
+
