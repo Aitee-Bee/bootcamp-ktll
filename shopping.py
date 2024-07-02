39eacd5fa05311c79b07d8fcd5cd6daac9e26232
Itoro Eti-mfon <etimfonitoro@gmail.com>
2024-07-02 10:10:31 +0100

    addition to shopping mall

--- shopping.py
+++ shopping.py
@@ -1,29 +1,26 @@
 shopping_list = []
-print("Welcome Itoro's shopping mall")
+
+print("Welcome to Itoro's Shopping Mall")
+
 while True:
-    choice = input(
-        "Enter 0-to-exit; 1-to add item; 2-to remove item; 3-to view items in your cart "
+    choice = int(
+        input(
+            "Enter 0- to exit; 1-to add an item; 2- to remove an item; 3- to view cart "
+        )
     )
 
-    if int(choice) == 0:
-    choice = int(input("Enter 0-to-exit; 1-to add item; 2-to remove item; 3-to view items in your cart "))
-    
     if choice == 0:
         break
 
     if choice == 1:
-        print("You can only add 5 items.")
-        for i in range(1, 6):
+        print("You can only add 5 items")
         cart = {}
-        for i in range(1,6):
-            item = input("Enter an item: ")
+        for i in range(1, 6):
+            item = input("Enter an item ")
             cart[f"item{i}"] = item
             print(f"You have {i} item in the cart")
-        print(f"Here are the items in your cart {shopping_list}")
-        checkout = int(input("Do you want to checkout now? Press 5"))
-    elif int(choice) == 2:
         shopping_list.append(cart)
-        
+
         print(f"Here are the items in your cart: {shopping_list}")
         # checkout = int(input("Do you want to checkout now? Press 5"))
 
@@ -37,21 +34,7 @@ while True:
         elif int(del_choice) == 0:
             shopping_list.clear()
             print("Your cart is empty.")
+
     elif choice == 3:
         for item in shopping_list:
             print(item)
-
-
-"""
-DRY - Don't Repeat Yourself
-"""
-
-cart = [
-    {
-        "item_one": "spoon",
-        "item_two": "yoghurt",
-        "item_three": "towels",
-        "item_four": "biscuit",
-        "item_five": "juice",
-    }
-]
